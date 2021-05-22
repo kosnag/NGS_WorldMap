fb2bf88d1e1dac07bbb9833decb1f24f19a47314
kosnag <63012171+kosnag@users.noreply.github.com>
2021-05-23 01:31:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ VK.Widgets.Group("vk_groups2",{mode:3,width:"auto",color1:'FFFFFF',color2:'00000
 VK.Widgets.Group("vk_groups3",{mode:3,width:"auto",color1:'FFFFFF',color2:'000000',color3: '0275d8'},28920792);
 
 // MAP
-var map_url = "/images/map.jpg";
+var map_url = "https://kosnag.github.io/NGS_InterMap/images/map.jpg";
 var map = L.map("map",{crs:L.CRS.Simple,minZoom:-4});
 var map_bounds = [[0,0],[12288,12288]];
 var map_img = L.imageOverlay(map_url,map_bounds).addTo(map);map.fitBounds(map_bounds);
@@ -23,8 +23,8 @@ $.getJSON('json/MarkersMain.json', function(data) {
 
 // ICONS
 var Main_Icons = L.Icon.extend({options: {iconSize:[40,40],iconAnchor:[20,40],popupAnchor:[0,-25]}});
-var CentTower = new Main_Icons({iconUrl: '/images/marks/cent_tower.png'}),
-	Cocoon = new Main_Icons({iconUrl: '/images/marks/cocoon.png'}),
+var CentTower = new Main_Icons({iconUrl: 'images/marks/cent_tower.png'}),
+	Cocoon = new Main_Icons({iconUrl: 'images/marks/cocoon.png'}),
 	Mag = new Main_Icons({iconUrl: 'images/marks/mag.png'}),
 	Ryuker = new Main_Icons({iconUrl: 'images/marks/ryuker.png'}),
 	Tower = new Main_Icons({iconUrl: 'images/marks/tower.png'}),
@@ -38,7 +38,7 @@ var CentTower = new Main_Icons({iconUrl: '/images/marks/cent_tower.png'}),
 // MARKERS UI
 var marker = 
 	L.marker([3599,3860], {icon: CentTower}).bindPopup("Central Tower (Aelio)").addTo(map);
-	L.marker([3333,2606], {icon: Mag}).bindPopup("Mag").addTo(map);
-	L.marker([4436,4107], {icon: Mag}).bindPopup("Mag").addTo(map);
-	L.marker([1003,4935], {icon: Mag}).bindPopup("Mag").addTo(map);
+	L.marker([3333,2606], {icon: Cocoon}).bindPopup("Cocoon").addTo(map);
+	L.marker([4436,4107], {icon: Cocoon}).bindPopup("Cocoon").addTo(map);
+	L.marker([1003,4935], {icon: Cocoon}).bindPopup("Cocoon").addTo(map);
 
