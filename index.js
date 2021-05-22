f16fc9fd32f3175df1c66b0aec5dedda1ddaa833
kosnag <63012171+kosnag@users.noreply.github.com>
2021-05-23 01:30:39 +0300

    test

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var map_img = L.imageOverlay(map_url,map_bounds).addTo(map);map.fitBounds(map_bo
 
 // JSON IMPORT
 function onEachFeature(feature, layer) {layer.bindPopup(feature.properties.text);}
-$.getJSON('/json/MarkersMain.json', function(data) {
+$.getJSON('json/MarkersMain.json', function(data) {
 	L.geoJson(data, {
 		pointToLayer: function(feature, latlng) {
 			return L.marker(latlng, {icon: TEST});
@@ -25,9 +25,9 @@ $.getJSON('/json/MarkersMain.json', function(data) {
 var Main_Icons = L.Icon.extend({options: {iconSize:[40,40],iconAnchor:[20,40],popupAnchor:[0,-25]}});
 var CentTower = new Main_Icons({iconUrl: '/images/marks/cent_tower.png'}),
 	Cocoon = new Main_Icons({iconUrl: '/images/marks/cocoon.png'}),
-	Mag = new Main_Icons({iconUrl: '/images/marks/mag.png'}),
-	Ryuker = new Main_Icons({iconUrl: '/images/marks/ryuker.png'}),
-	Tower = new Main_Icons({iconUrl: '/images/marks/tower.png'}),
+	Mag = new Main_Icons({iconUrl: 'images/marks/mag.png'}),
+	Ryuker = new Main_Icons({iconUrl: 'images/marks/ryuker.png'}),
+	Tower = new Main_Icons({iconUrl: 'images/marks/tower.png'}),
 	UQ = new Main_Icons({iconUrl: '/images/marks/urgent.png'}),
 	City = new Main_Icons({iconUrl: '/images/marks/city.png'}),
 	Hut = new Main_Icons({iconUrl: '/images/marks/hut.png'}),
@@ -38,5 +38,7 @@ var CentTower = new Main_Icons({iconUrl: '/images/marks/cent_tower.png'}),
 // MARKERS UI
 var marker = 
 	L.marker([3599,3860], {icon: CentTower}).bindPopup("Central Tower (Aelio)").addTo(map);
-	L.marker([4618,1570], {icon: Mag}).bindPopup("Mag").addTo(map);
+	L.marker([3333,2606], {icon: Mag}).bindPopup("Mag").addTo(map);
+	L.marker([4436,4107], {icon: Mag}).bindPopup("Mag").addTo(map);
+	L.marker([1003,4935], {icon: Mag}).bindPopup("Mag").addTo(map);
 
