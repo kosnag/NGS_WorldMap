57c0a2f5478f595ff6f5b9c37a612a45145d5893
kosnag <63012171+kosnag@users.noreply.github.com>
2021-05-22 16:12:18 +0300

    json test 4

--- js/intermap.js
+++ js/intermap.js
@@ -6,23 +6,24 @@ var map_img = L.imageOverlay(map_url, map_bounds).addTo(map);
 map.fitBounds(map_bounds);
 
 // JSON IMPORT
+$.getJSON("js/json/markers.json",function(data){L.geoJson(data).addTo(map);});
 
-						
 // ICONS
-var Main_Icon = L.Icon.extend({options: {iconSize: [40,40],iconAnchor: [20,40],popupAnchor: [40,0]}});
+var Main_Icons = L.Icon.extend({options: {iconSize: [40,40],iconAnchor: [20,40],popupAnchor: [40,0]}});
 									
-var CentTower = new Main_Icon({iconUrl: 'images/marks/cent_tower.png'}),
-	Cocoon = new Main_Icon({iconUrl: 'images/marks/cocoon.png'}),
-	Mag = new Main_Icon({iconUrl: 'images/marks/mag.png'}),
-	Ryuker = new Main_Icon({iconUrl: 'images/marks/ryuker.png'}),
-	Tower = new Main_Icon({iconUrl: 'images/marks/tower.png'}),
-	UQ = new Main_Icon({iconUrl: 'images/marks/urgent.png'}),
-	City = new Main_Icon({iconUrl: 'images/marks/city.png'}),
-	Hut = new Main_Icon({iconUrl: 'images/marks/hut.png'}),
-	Dungeon = new Main_Icon({iconUrl: 'images/marks/dungeon.png'}),
-	Fort = new Main_Icon({iconUrl: 'images/marks/fort.png'});
+var CentTower = new Main_Icons({iconUrl: 'images/marks/cent_tower.png'}),
+	Cocoon = new Main_Icons({iconUrl: 'images/marks/cocoon.png'}),
+	Mag = new Main_Icons({iconUrl: 'images/marks/mag.png'}),
+	Ryuker = new Main_Icons({iconUrl: 'images/marks/ryuker.png'}),
+	Tower = new Main_Icons({iconUrl: 'images/marks/tower.png'}),
+	UQ = new Main_Icons({iconUrl: 'images/marks/urgent.png'}),
+	City = new Main_Icons({iconUrl: 'images/marks/city.png'}),
+	Hut = new Main_Icons({iconUrl: 'images/marks/hut.png'}),
+	Dungeon = new Main_Icons({iconUrl: 'images/marks/dungeon.png'}),
+	Fort = new Main_Icons({iconUrl: 'images/marks/fort.png'});
 											
 // MARKERS UI
 var marker = 
 	L.marker([3585,4050], {icon: CentTower}).bindPopup("Central Tower (Aelio)").addTo(map);
 	L.marker([12288-7670,1570], {icon: Mag}).bindPopup("Mag").addTo(map);
+
