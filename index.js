56ce0af6d45af0c31bf0bca9bdc9d79d1d19837c
kosnag <63012171+kosnag@users.noreply.github.com>
2021-05-23 01:34:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ VK.Widgets.Group("vk_groups2",{mode:3,width:"auto",color1:'FFFFFF',color2:'00000
 VK.Widgets.Group("vk_groups3",{mode:3,width:"auto",color1:'FFFFFF',color2:'000000',color3: '0275d8'},28920792);
 
 // MAP
-var map_url = "https://kosnag.github.io/NGS_InterMap/images/map.jpg";
+var map_url = "images/map.jpg";
 var map = L.map("map",{crs:L.CRS.Simple,minZoom:-4});
 var map_bounds = [[0,0],[12288,12288]];
 var map_img = L.imageOverlay(map_url,map_bounds).addTo(map);map.fitBounds(map_bounds);
@@ -28,11 +28,11 @@ var CentTower = new Main_Icons({iconUrl: 'images/marks/cent_tower.png'}),
 	Mag = new Main_Icons({iconUrl: 'images/marks/mag.png'}),
 	Ryuker = new Main_Icons({iconUrl: 'images/marks/ryuker.png'}),
 	Tower = new Main_Icons({iconUrl: 'images/marks/tower.png'}),
-	UQ = new Main_Icons({iconUrl: '/images/marks/urgent.png'}),
-	City = new Main_Icons({iconUrl: '/images/marks/city.png'}),
-	Hut = new Main_Icons({iconUrl: '/images/marks/hut.png'}),
-	Dungeon = new Main_Icons({iconUrl: '/images/marks/dungeon.png'}),
-	Fort = new Main_Icons({iconUrl: '/images/marks/fort.png'}),
+	UQ = new Main_Icons({iconUrl: 'images/marks/urgent.png'}),
+	City = new Main_Icons({iconUrl: 'images/marks/city.png'}),
+	Hut = new Main_Icons({iconUrl: 'images/marks/hut.png'}),
+	Dungeon = new Main_Icons({iconUrl: 'images/marks/dungeon.png'}),
+	Fort = new Main_Icons({iconUrl: 'images/marks/fort.png'}),
 	TEST = new Main_Icons({iconUrl: 'https://cdn.discordapp.com/emojis/763104888732647434.png'});
 											
 // MARKERS UI
