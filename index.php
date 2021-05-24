c7358b5e59373e2a582ea9b56339a72b79a8d09f
kosnag <63012171+kosnag@users.noreply.github.com>
2021-05-24 18:14:10 +0300

    bug fix

--- index.php
+++ index.php
@@ -48,19 +48,18 @@ switch ($_GET['QUERY_STRING']) {
 		<link href="css/styles.css" rel="stylesheet">
 		<link href="css/leaflet.css" rel="stylesheet">
 		<script src="js/jquery-3.4.1.min.js"></script>
-		<!--<script src="js/popper.min.js"></script>-->
 		<script src="js/bootstrap.bundle.js"></script>
 		<script src="js/leaflet.js"></script>
 		<script src="https://vk.com/js/api/openapi.js?169"></script>
 		<link rel="icon" href="images/favicon.png" type="image/x-icon">
 	</head>
 	<body class="bg-body">
-		<nav class="navbar navbar-expand-sm navbar-dark nav-custom">		
+		<nav class="navbar navbar-expand-lg navbar-dark nav-custom">		
 			<div class="container">
-				<a class="navbar-brand text-white" href="#"><h4><?=$localisation['UI']['title']?></h4></a>
+				<a class="navbar-brand" href="#"><h4><?=$localisation['UI']['title']?></h4></a>
 				<ul class="navbar-nav ml-auto">
 					<li class="nav-item">
-						<a class="nav-link text-white disabled" target="_blank" href="#">Credits</a>
+						<a class="nav-link text-white-50 disabled" target="_blank" href="#">Credits</a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link text-white" target="_blank" href="https://docs.google.com/spreadsheets/d/12lKTrbbyHszz9TTfmvwRlRZe7MLI9nFL8XVAcIkynlU/edit?usp=sharing">Google Sheets</a>
@@ -68,28 +67,29 @@ switch ($_GET['QUERY_STRING']) {
 					<li class="nav-item">
 						<a class="nav-link text-white" target="_blank" href="https://github.com/kosnag/NGS_InterMap">Github</a>
 					</li>
-					<li class="nav-item">
-						<div class="dropdown">
-							<a class="nav-link text-white dropdown-toggle" data-toggle="dropdown" href="#"><?=$localisation['UI']['languages']['switcher']?></a>
-							<div class="dropdown-menu dropdown-menu-right">
-								<a class="dropdown-item" href="/?lang=en_gl"><img src="images/flags/usa.svg" class="lang-flags"> <img src="images/flags/uk.svg" class="lang-flags"> <?=$localisation['UI']['languages']['en_gl']?></a>
-								<a class="dropdown-item" href="/?lang=en_al"><img src="images/flags/usa.svg" class="lang-flags"> <img src="images/flags/uk.svg" class="lang-flags"> <?=$localisation['UI']['languages']['en_al']?></a>
-								<a class="dropdown-item" href="/?lang=jp"><img src="images/flags/jp.svg" class="lang-flags"> <?=$localisation['UI']['languages']['jp']?></a>
-								<a class="dropdown-item" href="/?lang=ru"><img src="images/flags/ru.svg" class="lang-flags"> <?=$localisation['UI']['languages']['ru']?></a>
-								<a class="dropdown-item" href="/?lang=kr"><img src="images/flags/kr.svg" class="lang-flags"> <?=$localisation['UI']['languages']['kr']?></a>
-							</div>
-						</div>
+					<li class="nav-item dropdown">
+						<a class="nav-link text-white dropdown-toggle" href="#" id="langList" data-bs-toggle="dropdown" aria-expanded="false"><?=$localisation['UI']['languages']['switcher']?></a>
+						<ul class="dropdown-menu" aria-labelledby="langList">
+							<li><a class="dropdown-item" href="/?lang=en_gl"><img src="images/flags/usa.svg" class="lang-flags"> <img src="images/flags/uk.svg" class="lang-flags"> <?=$localisation['UI']['languages']['en_gl']?></a></li>
+							<li><a class="dropdown-item" href="/?lang=en_al"><img src="images/flags/usa.svg" class="lang-flags"> <img src="images/flags/uk.svg" class="lang-flags"> <?=$localisation['UI']['languages']['en_al']?></a></li>
+							<li><a class="dropdown-item" href="/?lang=jp"><img src="images/flags/jp.svg" class="lang-flags"> <?=$localisation['UI']['languages']['jp']?></a></li>
+							<li><a class="dropdown-item" href="/?lang=ru"><img src="images/flags/ru.svg" class="lang-flags"> <?=$localisation['UI']['languages']['ru']?></a></li>
+							<li><a class="dropdown-item" href="/?lang=kr"><img src="images/flags/kr.svg" class="lang-flags"> <?=$localisation['UI']['languages']['kr']?></a></li>
+						</ul>
 					</li>
 				</ul>
 			</div>
 		</nav>
 		<div class="container alert alert-danger mt-3" role="alert">
-			<h4 class="alert-heading">WORK IN PROGRESS <span class="badge badge-danger">SPOILER ALERT</span></h4>
+			<h4 class="alert-heading">WORK IN PROGRESS <span class="badge bg-danger">SPOILER ALERT</span></h4>
 			<p>This interactive map are corrently in development. Also something maybe will be a spoiler for <b>Phantasy Star Online 2: New Genesis</b> because used data from datamining.</p>
 			<p>When map will be ready, all spoiler content will be hidden from map, dont worry.</p>
 			<hr>
-			<p class="mb-0">If you want to help me, please contact with me in <a href="https://twitter.com/kosnag" target="_blank" style="">Twitter</a> or <a href="#" data-toggle="tooltip" data-placement="bottom" data-html="true" title="<img class='d-block m-1 mx-auto' src='https://cdn.discordapp.com/attachments/418391120914022401/846113050695827456/unknown.png' height='100'>">Discord (kosnag#1730)</a>. I will be very happy, if you help me! <a href="#" data-toggle="tooltip" data-placement="bottom" data-html="true" title="<img class='d-block m-1 mx-auto' src='https://cdn.discordapp.com/attachments/762945798064570398/827230435662233671/2021-04-01_10-08-46-250_Talim_-_Air.png' height='100'>">Take a hug by Matoi!</a></p>
-			<script>$(function(){$('[data-toggle="tooltip"]').tooltip()})</script>
+			<p class="mb-0">If you want to help me, please contact with me in <a href="https://twitter.com/kosnag" target="_blank" style="">Twitter</a> or <a href="#" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-html="true" title="<img class='d-block m-1 mx-auto' src='https://cdn.discordapp.com/attachments/418391120914022401/846113050695827456/unknown.png' height='100'>">Discord (kosnag#1730)</a>. I will be very happy, if you help me! <a href="#" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-html="true" title="<img class='d-block m-1 mx-auto' src='https://cdn.discordapp.com/attachments/762945798064570398/827230435662233671/2021-04-01_10-08-46-250_Talim_-_Air.png' height='100'>">Take a hug by Matoi!</a></p>
+			<script>
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {return new bootstrap.Tooltip(tooltipTriggerEl)})
+			</script>
 		</div>
 		<div class="container mb-3">
 			<div class="row">
