c95bf0736722f5f8a857fc99b21f64ec83b88c81
kosnag <63012171+kosnag@users.noreply.github.com>
2021-05-24 11:14:32 +0300

    a

---
+++ en_gl/index.php
@@ -0,0 +1,140 @@
+<?php include 'lang.php';?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title><?=$localisation['UI']['title']?></title>
+		<link href="../css/bootstrap-4.4.1.css" rel="stylesheet">
+		<link href="../css/styles.css" rel="stylesheet">
+		<link href="../css/leaflet.css" rel="stylesheet">
+		<script src="../js/jquery-3.4.1.min.js"></script>
+		<script src="../js/popper.min.js"></script>
+		<script src="../js/bootstrap-4.4.1.js"></script>
+		<script src="../js/leaflet.js"></script>
+		<script src="https://vk.com/js/api/openapi.js?169"></script>
+		<link rel="icon" href="./images/favicon.png" type="image/x-icon">
+	</head>
+	<body class="bg-body">
+		<nav class="navbar navbar-expand-sm navbar-dark nav-custom">		
+			<div class="container">
+				<a class="navbar-brand text-white" href="#"><h4><?=$localisation['UI']['title']?></h4></a>
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a id="credits" class="nav-link text-white disabled" target="_blank" href="#">Credits</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link text-white" target="_blank" href="https://docs.google.com/spreadsheets/d/12lKTrbbyHszz9TTfmvwRlRZe7MLI9nFL8XVAcIkynlU/edit?usp=sharing">Google Sheets</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link text-white" target="_blank" href="https://github.com/kosnag/NGS_InterMap">Github</a>
+					</li>
+					<li class="nav-item">
+						<div class="dropdown">
+							<a id="langs" class="nav-link text-white dropdown-toggle" data-toggle="dropdown" href="#"><?=$localisation['UI']['languages']['switcher']?></a>
+							<div class="dropdown-menu dropdown-menu-right">
+								<a class="dropdown-item" href="#"><img src="images/flags/usa.svg" class="lang-flags"> <img src="images/flags/uk.svg" class="lang-flags"> <?=$localisation['UI']['languages']['en_gl']?></a>
+								<a class="dropdown-item" href="#"><img src="images/flags/usa.svg" class="lang-flags"> <img src="images/flags/uk.svg" class="lang-flags"> <?=$localisation['UI']['languages']['en_al']?></a>
+								<a class="dropdown-item" href="#"><img src="images/flags/jp.svg" class="lang-flags"> <?=$localisation['UI']['languages']['jp']?></a>
+								<a class="dropdown-item" href="#"><img src="images/flags/ru.svg" class="lang-flags"> <?=$localisation['UI']['languages']['ru']?></a>
+								<a class="dropdown-item" href="#"><img src="images/flags/kr.svg" class="lang-flags"> <?=$localisation['UI']['languages']['kr']?></a>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<div class="container alert alert-danger mt-3" role="alert">
+			<h4 class="alert-heading">WORK IN PROGRESS <span class="badge badge-danger">SPOILER ALERT</span></h4>
+			<p>This interactive map are corrently in development. Also something maybe will be a spoiler for <b>Phantasy Star Online 2: New Genesis</b> because used data from datamining.</p>
+			<p>When map will be ready, all spoiler content will be hidden from map, dont worry.</p>
+			<hr>
+			<p class="mb-0">If you want to help me, please contact with me in <a href="https://twitter.com/kosnag" target="_blank" style="">Twitter</a> or <a href="#" data-toggle="tooltip" data-placement="bottom" data-html="true" title="<img class='d-block m-1 mx-auto' src='https://cdn.discordapp.com/attachments/418391120914022401/846113050695827456/unknown.png' height='100'>">Discord (kosnag#1730)</a>. I will be very happy, if you help me! <a href="#" data-toggle="tooltip" data-placement="bottom" data-html="true" title="<img class='d-block m-1 mx-auto' src='https://cdn.discordapp.com/attachments/762945798064570398/827230435662233671/2021-04-01_10-08-46-250_Talim_-_Air.png' height='100'>">Take a hug by Matoi!</a></p>
+			<script>$(function(){$('[data-toggle="tooltip"]').tooltip()})</script>
+		</div>
+		<div class="container mb-3">
+			<div class="row">
+				<div class="col-md-9 col-sm-12">
+					<div class="card card-body">
+						<center><div id="map"></div></center>
+						<script src="../js/map.js"></script>
+						<script>
+var marker = 
+// MARKERS
+	L.marker([3599,3860],{icon:CentTower}).bindPopup("<?=$localisation['markers']['centtower']['1']?>").addTo(map);
+								
+// COCOONS
+	L.marker([3333,2606],{icon:Cocoon}).bindPopup("<?=$localisation['markers']['cocoon']['1']?>").addTo(map);
+	L.marker([5521,2442],{icon:Cocoon}).bindPopup("<?=$localisation['markers']['cocoon']['2']?>").addTo(map);
+	L.marker([4436,4107],{icon:Cocoon}).bindPopup("<?=$localisation['markers']['cocoon']['3']?>").addTo(map);
+	L.marker([1003,4935],{icon:Cocoon}).bindPopup("<?=$localisation['markers']['cocoon']['4']?>").addTo(map);
+	//L.marker([926,2327],{icon:Cocoon}).bindPopup("<?=$localisation['markers']['cocoon']['5']?>").addTo(map);
+	//L.marker([5248,217],{icon:Cocoon}).bindPopup("<?=$localisation['markers']['cocoon']['6']?>").addTo(map);
+	//L.marker([4965,5879],{icon:Cocoon}).bindPopup("<?=$localisation['markers']['cocoon']['7']?>").addTo(map);
+	//L.marker([5813,8934],{icon:Cocoon}).bindPopup("<?=$localisation['markers']['cocoon']['8']?>").addTo(map);
+	//L.marker([4979,10081],{icon:Cocoon}).bindPopup("<?=$localisation['markers']['cocoon']['9']?>").addTo(map);
+	//L.marker([4577,8717],{icon:Cocoon}).bindPopup("<?=$localisation['markers']['cocoon']['10']?>").addTo(map);
+	//L.marker([4486,10805],{icon:Cocoon}).bindPopup("<?=$localisation['markers']['cocoon']['11']?>").addTo(map);
+	//L.marker([4511,8352],{icon:Cocoon}).bindPopup("<?=$localisation['markers']['cocoon']['12']?>").addTo(map);
+	//L.marker([2605,8441],{icon:Cocoon}).bindPopup("<?=$localisation['markers']['cocoon']['13']?>").addTo(map);
+	//L.marker([1500,8394],{icon:Cocoon}).bindPopup("<?=$localisation['markers']['cocoon']['14']?>").addTo(map);
+	//L.marker([1726,10745],{icon:Cocoon}).bindPopup("<?=$localisation['markers']['cocoon']['15']?>").addTo(map);
+							
+								
+	L.marker([4755,1641],{icon:Mag}).bindPopup("<?=$localisation['markers']['mag']['1']?>").addTo(map);
+							
+	L.marker([1547,4074],{icon:Ryuker}).bindPopup("<?=$localisation['markers']['ryuker']['1']?>").addTo(map);
+	L.marker([2562,4171],{icon:Ryuker}).bindPopup("<?=$localisation['markers']['ryuker']['2']?>").addTo(map);
+	L.marker([611,5299],{icon:Ryuker}).bindPopup("<?=$localisation['markers']['ryuker']['3']?>").addTo(map);
+	L.marker([3094,2699],{icon:Ryuker}).bindPopup("<?=$localisation['markers']['ryuker']['4']?>").addTo(map);
+	L.marker([3340,3515],{icon:Ryuker}).bindPopup("<?=$localisation['markers']['ryuker']['5']?>").addTo(map);
+	L.marker([4053,4510],{icon:Ryuker}).bindPopup("<?=$localisation['markers']['ryuker']['6']?>").addTo(map);
+	L.marker([4297,2252],{icon:Ryuker}).bindPopup("<?=$localisation['markers']['ryuker']['7']?>").addTo(map);
+	L.marker([5470,1511],{icon:Ryuker}).bindPopup("<?=$localisation['markers']['ryuker']['8']?>").addTo(map);
+							
+	L.marker([830,5506],{icon:Tower}).bindPopup("<?=$localisation['markers']['tower']['1']?>").addTo(map);
+							
+	L.marker([3483,3620],{icon:City}).bindPopup("<?=$localisation['markers']['city']['1']?>").addTo(map);
+	//L.marker([1803,1529],{icon:City}).bindPopup("<?=$localisation['markers']['city']['1']?>").addTo(map);
+							
+	//L.marker([3815,2925],{icon:Hut}).bindPopup("<?=$localisation['markers']['hut']['1']?>").addTo(map);
+	//L.marker([3056,1742],{icon:Hut}).bindPopup("<?=$localisation['markers']['hut']['2']?>").addTo(map);
+	//L.marker([1070,2054],{icon:Hut}).bindPopup("<?=$localisation['markers']['hut']['3']?>").addTo(map);
+	//L.marker([7822,3428],{icon:Hut}).bindPopup("<?=$localisation['markers']['hut']['4']?>").addTo(map);
+							
+	L.marker([8492,8492],{icon:TEST}).bindPopup("<?=$localisation['markers']['urgent']['1']?>").addTo(map);
+							
+	L.marker([1337,1337],{icon:TEST}).bindPopup("<?=$localisation['markers']['dungeon']['1']?>").addTo(map);
+						</script>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-12">
+                    <div class="card">
+						<center>
+							<h4 id="sorting-title" style="margin-top:10px;margin-bottom:10px"><?=$localisation['UI']['sorting']['title']?></h4>
+						</center>
+						<a id="sorting-1" class="btn btn-primary mb-2 mr-3 ml-3 disabled"><?=$localisation['UI']['sorting']['1']?></a>
+						<a id="sorting-2" class="btn btn-primary mb-2 mr-3 ml-3 disabled"><?=$localisation['UI']['sorting']['2']?></a>
+						<a id="sorting-3" class="btn btn-primary mb-2 mr-3 ml-3 disabled"><?=$localisation['UI']['sorting']['3']?></a>
+						<a id="sorting-4" class="btn btn-primary mb-2 mr-3 ml-3 disabled"><?=$localisation['UI']['sorting']['4']?></a>
+						<a id="sorting-5" class="btn btn-primary mb-2 mr-3 ml-3 disabled"><?=$localisation['UI']['sorting']['5']?></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container mb-4">
+			<div class="card card-body">
+				<div class="row">
+					<div class="col-md-4 col-sm-12"><div id="vk_groups1"></div></div>
+					<div class="col-md-4 col-sm-12"><div id="vk_groups2"></div></div>
+					<div class="col-md-4 col-sm-12"><div id="vk_groups3"></div></div>
+					<script>
+						VK.Widgets.Group("vk_groups1",{mode:3,width:"auto",color1:'FFFFFF',color2:'000000',color3: '0275d8'},144825795);
+						VK.Widgets.Group("vk_groups2",{mode:3,width:"auto",color1:'FFFFFF',color2:'000000',color3: '0275d8'},200732667);
+						VK.Widgets.Group("vk_groups3",{mode:3,width:"auto",color1:'FFFFFF',color2:'000000',color3: '0275d8'},28920792);
+					</script>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
--- js/langs.js
+++ js/langs.js
@@ -1,96 +1,83 @@
-var language; 
-function getLanguage() {
-	(localStorage.getItem('language') == null) ? setLanguage('en_global') : false;
-	$.ajax({ 
-		url:  'json/langs/' +  localStorage.getItem('language') + '.json', 
-		dataType: 'json', async: false, dataType: 'json', 
-		success: function (lang) { language = lang } 
-	});
-}
-function setLanguage(lang) {
-	localStorage.setItem('language', lang);
-}
+var Credits = "TEST1"
+var Langs = "TEST1"
+var SortingTitle= "TEST1"
+var Sorting1 = "TEST1"
+var Sorting2 = "TEST1"
+var Sorting3 = "TEST1"
+var Sorting4 = "TEST1"
+var Sorting5 = "TEST1"
+var Sorting6 = "TEST1"
+var Sorting7 = "TEST1"
 
-$(document).ready(function(){$('#credits').text(language.UI.credits);});
-$(document).ready(function(){$('#langs').text(language.UI.langs);});
-$(document).ready(function(){$('#sorting-title').text(language.UI.sorting.title);});
-$(document).ready(function(){$('#sorting-1').text(language.UI.sorting.1);});
-$(document).ready(function(){$('#sorting-2').text(language.UI.sorting.2);});
-$(document).ready(function(){$('#sorting-3').text(language.UI.sorting.3);});
-$(document).ready(function(){$('#sorting-4').text(language.UI.sorting.4);});
-$(document).ready(function(){$('#sorting-5').text(language.UI.sorting.5);});
-$(document).ready(function(){$('#sorting-6').text(language.UI.sorting.6);});
-$(document).ready(function(){$('#sorting-7').text(language.UI.sorting.7);});
+var Cocoon1 = "TEST1"
+var Cocoon2 = "TEST1"
+var Cocoon3 = "TEST1"
+var Cocoon4 = "TEST1"
+var Cocoon5 = "TEST1"
+var Cocoon6 = "TEST1"
+var Cocoon7 = "TEST1"
+var Cocoon8 = "TEST1"
+var Cocoon9 = "TEST1"
+var Cocoon10 = "TEST1"
+var Cocoon11 = "TEST1"
+var Cocoon12 = "TEST1"
+var Cocoon13 = "TEST1"
+var Cocoon14 = "TEST1"
+var Cocoon15 = "TEST1"
 
-$(document).ready(function(){$('#Cocoon1').text(language.Cocoon.1);});
-$(document).ready(function(){$('#Cocoon2').text(language.Cocoon.2);});
-$(document).ready(function(){$('#Cocoon3').text(language.Cocoon.3);});
-$(document).ready(function(){$('#Cocoon4').text(language.Cocoon.4);});
-$(document).ready(function(){$('#Cocoon5').text(language.Cocoon.5);});
-$(document).ready(function(){$('#Cocoon6').text(language.Cocoon.6);});
-$(document).ready(function(){$('#Cocoon7').text(language.Cocoon.7);});
-$(document).ready(function(){$('#Cocoon8').text(language.Cocoon.8);});
-$(document).ready(function(){$('#Cocoon9').text(language.Cocoon.9);});
-$(document).ready(function(){$('#Cocoon10').text(language.Cocoon.10);});
-$(document).ready(function(){$('#Cocoon11').text(language.Cocoon.11);});
-$(document).ready(function(){$('#Cocoon12').text(language.Cocoon.12);});
-$(document).ready(function(){$('#Cocoon13').text(language.Cocoon.13);});
-$(document).ready(function(){$('#Cocoon14').text(language.Cocoon.14);});
-$(document).ready(function(){$('#Cocoon15').text(language.Cocoon.15);});
+var Mag1 = "TEST1"
+var Mag2 = "TEST1"
+var Mag3 = "TEST1"
+var Mag4 = "TEST1"
 
-$(document).ready(function(){$('#Mag1').text(language.Mag.1);});
-$(document).ready(function(){$('#Mag2').text(language.Mag.2);});
-$(document).ready(function(){$('#Mag3').text(language.Mag.3);});
-$(document).ready(function(){$('#Mag4').text(language.Mag.4);});
+var Ryuker1 = "TEST1"
+var Ryuker2 = "TEST1"
+var Ryuker3 = "TEST1"
+var Ryuker4 = "TEST1"
+var Ryuker5 = "TEST1"
+var Ryuker6 = "TEST1"
+var Ryuker7 = "TEST1"
+var Ryuker8 = "TEST1"
+var Ryuker9 = "TEST1"
+var Ryuker10 = "TEST1"
+var Ryuker11 = "TEST1"
+var Ryuker12 = "TEST1"
+var Ryuker13 = "TEST1"
+var Ryuker14 = "TEST1"
+var Ryuker15 = "TEST1"
+var Ryuker16 = "TEST1"
+var Ryuker17 = "TEST1"
+var Ryuker18 = "TEST1"
+var Ryuker19 = "TEST1"
+var Ryuker20 = "TEST1"
 
-$(document).ready(function(){$('#Ryuker1').text(language.Ryuker.1);});
-$(document).ready(function(){$('#Ryuker2').text(language.Ryuker.2);});
-$(document).ready(function(){$('#Ryuker3').text(language.Ryuker.3);});
-$(document).ready(function(){$('#Ryuker4').text(language.Ryuker.4);});
-$(document).ready(function(){$('#Ryuker5').text(language.Ryuker.5);});
-$(document).ready(function(){$('#Ryuker6').text(language.Ryuker.6);});
-$(document).ready(function(){$('#Ryuker7').text(language.Ryuker.7);});
-$(document).ready(function(){$('#Ryuker8').text(language.Ryuker.8);});
-$(document).ready(function(){$('#Ryuker9').text(language.Ryuker.9);});
-$(document).ready(function(){$('#Ryuker10').text(language.Ryuker.10);});
-$(document).ready(function(){$('#Ryuker11').text(language.Ryuker.11);});
-$(document).ready(function(){$('#Ryuker12').text(language.Ryuker.12);});
-$(document).ready(function(){$('#Ryuker13').text(language.Ryuker.13);});
-$(document).ready(function(){$('#Ryuker14').text(language.Ryuker.14);});
-$(document).ready(function(){$('#Ryuker15').text(language.Ryuker.15);});
-$(document).ready(function(){$('#Ryuker16').text(language.Ryuker.16);});
-$(document).ready(function(){$('#Ryuker17').text(language.Ryuker.17);});
-$(document).ready(function(){$('#Ryuker18').text(language.Ryuker.18);});
-$(document).ready(function(){$('#Ryuker19').text(language.Ryuker.19);});
-$(document).ready(function(){$('#Ryuker20').text(language.Ryuker.20);});
+var Tower1= "TEST1"
+var Tower2= "TEST1"
+var Tower3= "TEST1"
+var Tower4= "TEST1"
+var Tower5= "TEST1"
+var Tower6= "TEST1"
+var Tower7= "TEST1"
 
-$(document).ready(function(){$('#Tower1').text(language.Tower.1);});
-$(document).ready(function(){$('#Tower2').text(language.Tower.2);});
-$(document).ready(function(){$('#Tower3').text(language.Tower.3);});
-$(document).ready(function(){$('#Tower4').text(language.Tower.4);});
-$(document).ready(function(){$('#Tower5').text(language.Tower.5);});
-$(document).ready(function(){$('#Tower6').text(language.Tower.6);});
-$(document).ready(function(){$('#Tower7').text(language.Tower.7);});
+var City1= "TEST1"
+var City2= "TEST1"
+var City3= "TEST1"
+var City4= "TEST1"
+var City5= "TEST1"
 
-$(document).ready(function(){$('#City1').text(language.City.1);});
-$(document).ready(function(){$('#City2').text(language.City.2);});
-$(document).ready(function(){$('#City3').text(language.City.3);});
-$(document).ready(function(){$('#City4').text(language.City.4);});
-$(document).ready(function(){$('#City5').text(language.City.5);});
+var Hut1= "TEST1"
+var Hut2= "TEST1"
+var Hut3= "TEST1"
+var Hut4= "TEST1"
 
-$(document).ready(function(){$('#Hut1').text(language.Hut.1);});
-$(document).ready(function(){$('#Hut2').text(language.Hut.2);});
-$(document).ready(function(){$('#Hut3').text(language.Hut.3);});
-$(document).ready(function(){$('#Hut4').text(language.Hut.4);});
+var Urgent1= "TEST1"
+var Urgent2= "TEST1"
+var Urgent3= "TEST1"
+var Urgent4= "TEST1"
+var Urgent5= "TEST1"
 
-$(document).ready(function(){$('#Urgent1').text(language.Urgent.1);});
-$(document).ready(function(){$('#Urgent2').text(language.Urgent.2);});
-$(document).ready(function(){$('#Urgent3').text(language.Urgent.3);});
-$(document).ready(function(){$('#Urgent4').text(language.Urgent.4);});
-$(document).ready(function(){$('#Urgent5').text(language.Urgent.5);});
-
-$(document).ready(function(){$('#Dungeon1').text(language.Dungeon.1);});
-$(document).ready(function(){$('#Dungeon2').text(language.Dungeon.2);});
-$(document).ready(function(){$('#Dungeon3').text(language.Dungeon.3);});
-$(document).ready(function(){$('#Dungeon4').text(language.Dungeon.4);});
-$(document).ready(function(){$('#Dungeon5').text(language.Dungeon.5);});
+var Dungeon1= "TEST1"
+var Dungeon2= "TEST1"
+var Dungeon3= "TEST1"
+var Dungeon4= "TEST1"
+var Dungeon5= "TEST1"
