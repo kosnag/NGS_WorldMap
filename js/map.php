faae444894b012f18f812b96592c1d54f6c49ed8
kosnag <63012171+kosnag@users.noreply.github.com>
2021-06-12 00:35:23 +0300

    debug popup with coordinates for materials

--- js/map.php
+++ js/map.php
@@ -299,7 +299,7 @@ $(document).ready(function(){
 			$("#redBox_On").click(function(){
 				if (redBoxToggle.length == 0) {
 					for (let i = 0; i < redBox.length; i++) {
-						redBoxToggle.push(L.circleMarker(redBox[i]["coordinates"],{<?echo $roundMarker['boxes']['red'];?>}).bindTooltip("<?=$localization['UI']['sorting']['boxes']['redBox']['name']?>"));
+						redBoxToggle.push(L.circleMarker(redBox[i]["coordinates"],{<?echo $roundMarker['boxes']['red'];?>}).bindTooltip("<?=$localization['UI']['sorting']['boxes']['redBox']['name']?>").bindPopup("YX: " + redBox[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < redBoxToggle.length; i++) {
@@ -316,7 +316,7 @@ $(document).ready(function(){
 			$("#whiteBox_On").click(function(){
 				if (whiteBoxToggle.length == 0) {
 					for (let i = 0; i < whiteBox.length; i++) {
-						whiteBoxToggle.push(L.circleMarker(whiteBox[i]["coordinates"],{<?echo $roundMarker['boxes']['white'];?>}).bindTooltip("<?=$localization['UI']['sorting']['boxes']['whiteBox']['name']?>"));
+						whiteBoxToggle.push(L.circleMarker(whiteBox[i]["coordinates"],{<?echo $roundMarker['boxes']['white'];?>}).bindTooltip("<?=$localization['UI']['sorting']['boxes']['whiteBox']['name']?>").bindPopup("YX: " + whiteBox[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < whiteBoxToggle.length; i++) {
@@ -333,7 +333,7 @@ $(document).ready(function(){
 			$("#goldBox_On").click(function(){
 				if (goldBoxToggle.length == 0) {
 					for (let i = 0; i < goldBox.length; i++) {
-						goldBoxToggle.push(L.circleMarker(goldBox[i]["coordinates"],{<?echo $roundMarker['boxes']['gold'];?>}).bindTooltip("<?=$localization['UI']['sorting']['boxes']['goldBox']['name']?>"));
+						goldBoxToggle.push(L.circleMarker(goldBox[i]["coordinates"],{<?echo $roundMarker['boxes']['gold'];?>}).bindTooltip("<?=$localization['UI']['sorting']['boxes']['goldBox']['name']?>").bindPopup("YX: " + goldBox[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < goldBoxToggle.length; i++) {
@@ -352,7 +352,7 @@ $(document).ready(function(){
 			$("#sautePeach_On").click(function(){
 				if (sautePeachToggle.length == 0) {
 					for (let i = 0; i < sautePeach.length; i++) {
-						sautePeachToggle.push(L.marker(sautePeach[i]["coordinates"],{icon:iconSautePeach}).bindTooltip("<?=$localization['UI']['sorting']['food']['sautePeach']['name']?>"));;
+						sautePeachToggle.push(L.marker(sautePeach[i]["coordinates"],{icon:iconSautePeach}).bindTooltip("<?=$localization['UI']['sorting']['food']['sautePeach']['name']?>").bindPopup("YX: " + sautePeach[i]["coordinates"]));;
 					}
 				}
 				for (let i = 0; i < sautePeachToggle.length; i++) {
@@ -365,11 +365,11 @@ $(document).ready(function(){
 				}
 			});
 		}
-		/*{ // Saute Banana
+		{ // Saute Banana
 			$("#sauteBanana_On").click(function(){
 				if (sauteBananaToggle.length == 0) {
 					for (let i = 0; i < sauteBanana.length; i++) {
-						sauteBananaToggle.push(L.marker(sauteBanana[i]["coordinates"],{icon:iconSautePeach}).bindTooltip("<?=$localization['UI']['sorting']['food']['sauteBanana']['name']?>"));;
+						sauteBananaToggle.push(L.marker(sauteBanana[i]["coordinates"],{icon:iconSautePeach}).bindTooltip("<?=$localization['UI']['sorting']['food']['sauteBanana']['name']?>").bindPopup("YX: " + sauteBanana[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < sauteBananaToggle.length; i++) {
@@ -381,12 +381,12 @@ $(document).ready(function(){
 					map.removeLayer(sauteBananaToggle[i]);
 				}
 			});
-		}*/
+		}
 		{ // Saute Apple
 			$("#sauteApple_On").click(function(){
 				if (sauteAppleToggle.length == 0) {
 					for (let i = 0; i < sauteApple.length; i++) {
-						sauteAppleToggle.push(L.marker(sauteApple[i]["coordinates"],{icon:iconSauteApple}).bindTooltip("<?=$localization['UI']['sorting']['food']['sauteApple']['name']?>"));;
+						sauteAppleToggle.push(L.marker(sauteApple[i]["coordinates"],{icon:iconSauteApple}).bindTooltip("<?=$localization['UI']['sorting']['food']['sauteApple']['name']?>").bindPopup("YX: " + sauteApple[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < sauteAppleToggle.length; i++) {
@@ -403,7 +403,7 @@ $(document).ready(function(){
 			$("#sautePear_On").click(function(){
 				if (sautePearToggle.length == 0) {
 					for (let i = 0; i < sautePear.length; i++) {
-						sautePearToggle.push(L.marker(sautePear[i]["coordinates"],{icon:iconSautePear}).bindTooltip("<?=$localization['UI']['sorting']['food']['sautePear']['name']?>"));;
+						sautePearToggle.push(L.marker(sautePear[i]["coordinates"],{icon:iconSautePear}).bindTooltip("<?=$localization['UI']['sorting']['food']['sautePear']['name']?>").bindPopup("YX: " + sautePear[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < sautePearToggle.length; i++) {
@@ -420,7 +420,7 @@ $(document).ready(function(){
 			$("#sodheTurbanShell_On").click(function(){
 				if (sodheTurbanShellToggle.length == 0) {
 					for (let i = 0; i < sodheTurbanShell.length; i++) {
-						sodheTurbanShellToggle.push(L.marker(sodheTurbanShell[i]["coordinates"],{icon:iconTEST}).bindTooltip("<?=$localization['UI']['sorting']['food']['sodheTurbanShell']['name']?>"));;
+						sodheTurbanShellToggle.push(L.marker(sodheTurbanShell[i]["coordinates"],{icon:iconTEST}).bindTooltip("<?=$localization['UI']['sorting']['food']['sodheTurbanShell']['name']?>").bindPopup("YX: " + sodheTurbanShell[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < sodheTurbanShellToggle.length; i++) {
@@ -437,7 +437,7 @@ $(document).ready(function(){
 			$("#sodheClam_On").click(function(){
 				if (sodheClamToggle.length == 0) {
 					for (let i = 0; i < sodheClam.length; i++) {
-						sodheClamToggle.push(L.marker(sodheClam[i]["coordinates"],{icon:iconSodheClam}).bindTooltip("<?=$localization['UI']['sorting']['food']['sodheClam']['name']?>"));;
+						sodheClamToggle.push(L.marker(sodheClam[i]["coordinates"],{icon:iconSodheClam}).bindTooltip("<?=$localization['UI']['sorting']['food']['sodheClam']['name']?>").bindPopup("YX: " + sodheClam[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < sodheClamToggle.length; i++) {
@@ -454,7 +454,7 @@ $(document).ready(function(){
 			$("#sohTurnip_On").click(function(){
 				if (sohTurnipToggle.length == 0) {
 					for (let i = 0; i < sohTurnip.length; i++) {
-						sohTurnipToggle.push(L.marker(sohTurnip[i]["coordinates"],{icon:iconSohTurnip}).bindTooltip("<?=$localization['UI']['sorting']['food']['sohTurnip']['name']?>"));;
+						sohTurnipToggle.push(L.marker(sohTurnip[i]["coordinates"],{icon:iconSohTurnip}).bindTooltip("<?=$localization['UI']['sorting']['food']['sohTurnip']['name']?>").bindPopup("YX: " + sohTurnip[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < sohTurnipToggle.length; i++) {
@@ -471,7 +471,7 @@ $(document).ready(function(){
 			$("#sohMushroom_On").click(function(){
 				if (sohMushroomToggle.length == 0) {
 					for (let i = 0; i < sohMushroom.length; i++) {
-						sohMushroomToggle.push(L.marker(sohMushroom[i]["coordinates"],{icon:iconSohMushroom}).bindTooltip("<?=$localization['UI']['sorting']['food']['sohMushroom']['name']?>"));;
+						sohMushroomToggle.push(L.marker(sohMushroom[i]["coordinates"],{icon:iconSohMushroom}).bindTooltip("<?=$localization['UI']['sorting']['food']['sohMushroom']['name']?>").bindPopup("YX: " + sohMushroom[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < sohMushroomToggle.length; i++) {
@@ -488,7 +488,7 @@ $(document).ready(function(){
 			$("#sodheCrab_On").click(function(){
 				if (sodheCrabToggle.length == 0) {
 					for (let i = 0; i < sodheCrab.length; i++) {
-						sodheCrabToggle.push(L.marker(sodheCrab[i]["coordinates"],{icon:iconSodheCrab}).bindTooltip("<?=$localization['UI']['sorting']['food']['sodheCrab']['name']?>"));;
+						sodheCrabToggle.push(L.marker(sodheCrab[i]["coordinates"],{icon:iconSodheCrab}).bindTooltip("<?=$localization['UI']['sorting']['food']['sodheCrab']['name']?>").bindPopup("YX: " + sodheCrab[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < sodheCrabToggle.length; i++) {
@@ -505,7 +505,7 @@ $(document).ready(function(){
 			$("#sodheLobster_On").click(function(){
 				if (sodheLobsterToggle.length == 0) {
 					for (let i = 0; i < sodheLobster.length; i++) {
-						sodheLobsterToggle.push(L.marker(sodheLobster[i]["coordinates"],{icon:iconSodheLobster}).bindTooltip("<?=$localization['UI']['sorting']['food']['sodheLobster']['name']?>"));;
+						sodheLobsterToggle.push(L.marker(sodheLobster[i]["coordinates"],{icon:iconSodheLobster}).bindTooltip("<?=$localization['UI']['sorting']['food']['sodheLobster']['name']?>").bindPopup("YX: " + sodheLobster[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < sodheLobsterToggle.length; i++) {
@@ -522,7 +522,7 @@ $(document).ready(function(){
 			$("#sohHerb_On").click(function(){
 				if (sohHerbToggle.length == 0) {
 					for (let i = 0; i < sohHerb.length; i++) {
-						sohHerbToggle.push(L.marker(sohHerb[i]["coordinates"],{icon:iconSohHerb}).bindTooltip("<?=$localization['UI']['sorting']['food']['sohHerb']['name']?>"));;
+						sohHerbToggle.push(L.marker(sohHerb[i]["coordinates"],{icon:iconSohHerb}).bindTooltip("<?=$localization['UI']['sorting']['food']['sohHerb']['name']?>").bindPopup("YX: " + sohHerb[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < sohHerbToggle.length; i++) {
@@ -539,7 +539,7 @@ $(document).ready(function(){
 			$("#sohTomato_On").click(function(){
 				if (sohTomatoToggle.length == 0) {
 					for (let i = 0; i < sohTomato.length; i++) {
-						sohTomatoToggle.push(L.marker(sohTomato[i]["coordinates"],{icon:iconTEST}).bindTooltip("<?=$localization['UI']['sorting']['food']['sohTomato']['name']?>"));;
+						sohTomatoToggle.push(L.marker(sohTomato[i]["coordinates"],{icon:iconTEST}).bindTooltip("<?=$localization['UI']['sorting']['food']['sohTomato']['name']?>").bindPopup("YX: " + sohTomato[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < sohTomatoToggle.length; i++) {
@@ -558,7 +558,7 @@ $(document).ready(function(){
 			$("#monotite_On").click(function(){
 				if (monotiteToggle.length == 0) {
 					for (let i = 0; i < monotite.length; i++) {
-						monotiteToggle.push(L.marker(monotite[i]["coordinates"],{icon:iconMonotite}).bindTooltip("<?=$localization['UI']['sorting']['minerals']['monotite']['name']?>"));;
+						monotiteToggle.push(L.marker(monotite[i]["coordinates"],{icon:iconMonotite}).bindTooltip("<?=$localization['UI']['sorting']['minerals']['monotite']['name']?>").bindPopup("YX: " + monotite[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < monotiteToggle.length; i++) {
@@ -575,7 +575,7 @@ $(document).ready(function(){
 			$("#dualomite_On").click(function(){
 				if (dualomiteToggle.length == 0) {
 					for (let i = 0; i < dualomite.length; i++) {
-						dualomiteToggle.push(L.marker(dualomite[i]["coordinates"],{icon:iconDualomite}).bindTooltip("<?=$localization['UI']['sorting']['minerals']['dualomite']['name']?>"));;
+						dualomiteToggle.push(L.marker(dualomite[i]["coordinates"],{icon:iconDualomite}).bindTooltip("<?=$localization['UI']['sorting']['minerals']['dualomite']['name']?>").bindPopup("YX: " + dualomite[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < dualomiteToggle.length; i++) {
@@ -592,7 +592,7 @@ $(document).ready(function(){
 			$("#trinite_On").click(function(){
 				if (triniteToggle.length == 0) {
 					for (let i = 0; i < trinite.length; i++) {
-						triniteToggle.push(L.marker(trinite[i]["coordinates"],{icon:iconTrinite}).bindTooltip("<?=$localization['UI']['sorting']['minerals']['trinite']['name']?>"));;
+						triniteToggle.push(L.marker(trinite[i]["coordinates"],{icon:iconTrinite}).bindTooltip("<?=$localization['UI']['sorting']['minerals']['trinite']['name']?>").bindPopup("YX: " + trinite[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < triniteToggle.length; i++) {
@@ -609,7 +609,7 @@ $(document).ready(function(){
 			$("#PhotonChunk_On").click(function(){
 				if (PhotonChunkToggle.length == 0) {
 					for (let i = 0; i < PhotonChunk.length; i++) {
-						PhotonChunkToggle.push(L.marker(PhotonChunk[i]["coordinates"],{icon:iconPhotonChunk}).bindTooltip("<?=$localization['UI']['sorting']['minerals']['PhotonChunk']['name']?>"));;
+						PhotonChunkToggle.push(L.marker(PhotonChunk[i]["coordinates"],{icon:iconPhotonChunk}).bindTooltip("<?=$localization['UI']['sorting']['minerals']['PhotonChunk']['name']?>").bindPopup("YX: " + PhotonChunk[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < PhotonChunkToggle.length; i++) {
@@ -626,7 +626,7 @@ $(document).ready(function(){
 			$("#PhotonQuartz_On").click(function(){
 				if (PhotonQuartzToggle.length == 0) {
 					for (let i = 0; i < PhotonQuartz.length; i++) {
-						PhotonQuartzToggle.push(L.marker(PhotonQuartz[i]["coordinates"],{icon:iconPhotonQuartz}).bindTooltip("<?=$localization['UI']['sorting']['minerals']['PhotonQuartz']['name']?>"));;
+						PhotonQuartzToggle.push(L.marker(PhotonQuartz[i]["coordinates"],{icon:iconPhotonQuartz}).bindTooltip("<?=$localization['UI']['sorting']['minerals']['PhotonQuartz']['name']?>").bindPopup("YX: " + PhotonQuartz[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < PhotonQuartzToggle.length; i++) {
@@ -643,7 +643,7 @@ $(document).ready(function(){
 			$("#alphaReactor_On").click(function(){
 				if (alphaReactorToggle.length == 0) {
 					for (let i = 0; i < alphaReactor.length; i++) {
-						alphaReactorToggle.push(L.marker(alphaReactor[i]["coordinates"],{icon:iconAlphaReactor}).bindTooltip("<?=$localization['UI']['sorting']['other']['alphaReactor']['name']?>"));;
+						alphaReactorToggle.push(L.marker(alphaReactor[i]["coordinates"],{icon:iconAlphaReactor}).bindTooltip("<?=$localization['UI']['sorting']['other']['alphaReactor']['name']?>").bindPopup("YX: " + alphaReactor[i]["coordinates"]));
 					}
 				}
 				for (let i = 0; i < alphaReactorToggle.length; i++) {
