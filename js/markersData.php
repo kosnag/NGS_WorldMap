627e7e496e2fb0c2f46d0c5df7d5e04e1e99d916
kosnag <63012171+kosnag@users.noreply.github.com>
2021-06-16 00:23:47 +0300

    Datapods locations

--- js/markersData.php
+++ js/markersData.php
@@ -436,3 +436,9 @@ include_once "../lang_packs/" . $lang . ".php";
 		{coordinates:[326,5358]}
 	];
 	let alphaReactorToggle = [];
+	
+// Datapods
+	let datapod = [
+		{coordinates:[5951,2027]},{coordinates:[5469,1807]},{coordinates:[5010,1784]},{coordinates:[4543,1866]},{coordinates:[4311,2597]},{coordinates:[4546,3703]},{coordinates:[4398,4815]},{coordinates:[4261,5546]},{coordinates:[3741,4245]},{coordinates:[3568,2944]},{coordinates:[3287,3078]},{coordinates:[2832,3130]},{coordinates:[2300,3762]},{coordinates:[2199,3209]},{coordinates:[2159,3675]},{coordinates:[1252,4144]},{coordinates:[641,3909]},{coordinates:[596,4157]},{coordinates:[2649,4174]},{coordinates:[1802,5408]}
+	];
+	let datapodToggle = [];
