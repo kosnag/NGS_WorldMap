dc26578ee2643914f3d5e6d74fab7e87201a12f7
kosnag <63012171+kosnag@users.noreply.github.com>
2021-06-17 20:45:01 +0300

    a

---
+++ js/localization.js
@@ -0,0 +1,7 @@
+function change_lang(lang){
+    var d = new Date();
+    d.setTime(d.getTime() + (24*60*60*365*10));
+    var expires = d.toUTCString();
+    document.cookie = "lang="+lang+"; expires = "+expires+"; path=/";
+    location.href="/";
+}
