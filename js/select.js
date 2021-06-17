bc0765850ff1acb37da4b251f14183aafe8ef037
kosnag <63012171+kosnag@users.noreply.github.com>
2021-06-17 19:34:40 +0300

    Moved JS to other file

---
+++ js/select.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $("#en").hover(function(){
+        $("title").text("PSO2NGS Interactive Map");
+        $("#title").text("PSO2NGS Interactive Map");
+        $("#lang").text("Choose a language");
+    });
+    $("#jp").hover(function(){
+        $("title").text("PSO2NGS相互マップ");
+        $("#title").text("PSO2NGS相互マップ");
+        $("#lang").text("言語を選択する");
+    });
+    $("#ru").hover(function(){
+        $("title").text("Интерактивная карта PSO2NGS");
+        $("#title").text("Интерактивная карта PSO2NGS");
+        $("#lang").text("Выберите язык");
+    });
+    $("#kr").hover(function(){
+        $("title" ).text("PSO2NGS 인터랙티브지도");
+        $("#title" ).text("PSO2NGS 인터랙티브지도");
+        $("#lang" ).text("언어 선택");
+    });
+    $("#pt").hover(function(){
+        $("title").text("Mapa Interactivo PSO2NGS");
+        $("#title").text("Mapa Interactivo PSO2NGS");
+        $("#lang").text("Escolha uma língua");
+    });
+});
