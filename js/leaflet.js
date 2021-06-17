98423d01a2cd9e9fb30c62db449a8859a9e94aa4
kosnag <63012171+kosnag@users.noreply.github.com>
2021-06-17 19:46:41 +0300

    a

--- js/leaflet.js
+++ js/leaflet.js
@@ -1,7 +1,3 @@
-/* @preserve
- * Leaflet 1.7.1, a JS library for interactive maps. http://leafletjs.com
- * (c) 2010-2019 Vladimir Agafonkin, (c) 2010-2011 CloudMade
- */
 ! function(t, i) {
     "object" == typeof exports && "undefined" != typeof module ? i(exports) : "function" == typeof define && define.amd ? define(["exports"], i) : i(t.L = {})
 }(this, function(t) {
