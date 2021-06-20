c302fbc8133c21407fc56de6faebcc2bd15ecc0f
kosnag <63012171+kosnag@users.noreply.github.com>
2021-06-20 14:34:05 +0300

    Trying to make multi-lang localization

--- js/localization.js
+++ js/localization.js
@@ -1,28 +1,3 @@
-function change_lang(lang){
-    var d = new Date();
-    d.setTime(d.getTime() + (24*60*60*365*10));
-    var expires = d.toUTCString();
-    document.cookie = "lang="+lang+"; expires = "+expires+"";
-    location.href="/";
-}
-
 $(document).ready(function(){
-    if (Cookies.get('lang') == 'en_gl') {
-        $("#lang_js").attr("src", "js/lang_packs/en_gl.js");
-    } else if (Cookies.get('lang') == 'en_al') {
-        $("#lang_js").attr("src", "js/lang_packs/en_al.js");
-    } else if (Cookies.get('lang') == 'jp') {
-        $("#lang_js").attr("src", "js/lang_packs/jp.js");
-    } else if (Cookies.get('lang') == 'ru') {
-        $("#lang_js").attr("src", "js/lang_packs/ru.js");
-    } else if (Cookies.get('lang') == 'kr') {
-        $("#lang_js").attr("src", "js/lang_packs/kr.js");
-    } else if (Cookies.get('lang') == 'pt') {
-        $("#lang_js").attr("src", "js/lang_packs/pt.js");
-    } else {
-        alert('NO COOKIE FOUND'); // i think how to realize loading of "select.html"
-    };
-
-
     $("title").text(langTitle);
 });
--- js/select.js
+++ js/select.js
@@ -1,27 +1,48 @@
 $(document).ready(function(){
-    $("#en").hover(function(){
-        $("title").text("PSO2NGS Interactive Map");
-        $("#title").text("PSO2NGS Interactive Map");
-        $("#lang").text("Choose a language");
+    $("#select-en").hover(function(){
+        $("#select-title").text("PSO2NGS Interactive Map");
+        $("#select-lang").text("Choose a language");
     });
-    $("#jp").hover(function(){
-        $("title").text("PSO2NGS相互マップ");
-        $("#title").text("PSO2NGS相互マップ");
-        $("#lang").text("言語を選択する");
+    $("#select-jp").hover(function(){
+        $("#select-title").text("PSO2NGS相互マップ");
+        $("#select-lang").text("言語を選択する");
     });
-    $("#ru").hover(function(){
-        $("title").text("Интерактивная карта PSO2NGS");
-        $("#title").text("Интерактивная карта PSO2NGS");
-        $("#lang").text("Выберите язык");
+    $("#select-ru").hover(function(){
+        $("#select-title").text("Интерактивная карта PSO2NGS");
+        $("#select-lang").text("Выберите язык");
     });
-    $("#kr").hover(function(){
-        $("title" ).text("PSO2NGS 인터랙티브지도");
-        $("#title" ).text("PSO2NGS 인터랙티브지도");
-        $("#lang" ).text("언어 선택");
+    $("#select-kr").hover(function(){
+        $("#select-title" ).text("PSO2NGS 인터랙티브지도");
+        $("#select-lang" ).text("언어 선택");
     });
-    $("#pt").hover(function(){
-        $("title").text("Mapa Interactivo PSO2NGS");
-        $("#title").text("Mapa Interactivo PSO2NGS");
-        $("#lang").text("Escolha uma língua");
+    $("#select-pt").hover(function(){
+        $("#select-title").text("Mapa Interactivo PSO2NGS");
+        $("#select-lang").text("Escolha uma língua");
     });
+});
+
+function change_lang(lang){
+    var d = new Date();
+    d.setTime(d.getTime() + (24*60*60*365*10));
+    var expires = d.toUTCString();
+    document.cookie = "lang="+lang+"; expires = "+expires+"";
+    location.href="/";
+}
+
+$(document).ready(function(){
+    if (Cookies.get('lang') == 'en_gl') {
+        $("#lang_js").attr("src", "js/lang_packs/en_gl.js");
+    } else if (Cookies.get('lang') == 'en_al') {
+        $("#lang_js").attr("src", "js/lang_packs/en_al.js");
+    } else if (Cookies.get('lang') == 'jp') {
+        $("#lang_js").attr("src", "js/lang_packs/jp.js");
+    } else if (Cookies.get('lang') == 'ru') {
+        $("#lang_js").attr("src", "js/lang_packs/ru.js");
+    } else if (Cookies.get('lang') == 'kr') {
+        $("#lang_js").attr("src", "js/lang_packs/kr.js");
+    } else if (Cookies.get('lang') == 'pt') {
+        $("#lang_js").attr("src", "js/lang_packs/pt.js");
+    } else {
+        alert('NO COOKIE FOUND'); // i think how to realize loading of "select.html"
+    };
 });
