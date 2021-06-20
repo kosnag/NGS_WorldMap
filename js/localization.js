62c69fb6911f8ff96ede4ecc6544920da2fbdb82
kosnag <63012171+kosnag@users.noreply.github.com>
2021-06-20 04:53:36 +0300

    why it doesn't works lol

--- js/localization.js
+++ js/localization.js
@@ -1,6 +1,3 @@
-// LOCALIZATION FILES
-/* */
-
 function change_lang(lang){
     var d = new Date();
     d.setTime(d.getTime() + (24*60*60*365*10));
@@ -9,23 +6,6 @@ function change_lang(lang){
     location.href="/";
 }
 
-/*
-if (Cookies.get('lang') == 'en_gl') {
-    $(document).ready(function(){$("#lang_js").attr("src", "js/lang_packs/en_gl.js");});
-} else if (Cookies.get('lang') == 'en_al') {
-    $(document).ready(function(){$("#lang_js").attr("src", "js/lang_packs/en_al.js");});
-} else if (Cookies.get('lang') == 'jp') {
-    $(document).ready(function(){$("#lang_js").attr("src", "js/lang_packs/jp.js");});
-} else if (Cookies.get('lang') == 'ru') {
-    $(document).ready(function(){$("#lang_js").attr("src", "js/lang_packs/ru.js");});
-} else if (Cookies.get('lang') == 'kr') {
-    $(document).ready(function(){$("#lang_js").attr("src", "js/lang_packs/kr.js");});
-} else if (Cookies.get('lang') == 'pt') {
-    $(document).ready(function(){$("#lang_js").attr("src", "js/lang_packs/pt.js");});
-} else {
-    alert('NO COOKIE FOUND');
-}
-*/
 $(document).ready(function(){
     if (Cookies.get('lang') == 'en_gl') {
         $("#lang_js").attr("src", "js/lang_packs/en_gl.js");
@@ -40,6 +20,9 @@ $(document).ready(function(){
     } else if (Cookies.get('lang') == 'pt') {
         $("#lang_js").attr("src", "js/lang_packs/pt.js");
     } else {
-        alert('NO COOKIE FOUND');
-    }
+        alert('NO COOKIE FOUND'); // i think how to realize loading of "select.html"
+    };
+
+
+    $("title").text(langTitle);
 });
