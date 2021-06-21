71ad2ac3b53298e000569a044b81a112ac85d010
kosnag <63012171+kosnag@users.noreply.github.com>
2021-06-21 19:15:52 +0300

    Rewrite try number 2
    by using SVGvsevolod lib

---
+++ js/map.main.js
@@ -0,0 +1,8 @@
+var map_app = new nekoapp({
+    applicationInfo: {
+        nekoappID : "18",
+        applicationTitle : "PSO2NGS Interactive Map",
+        applicationVersion : "v0.8492",
+        applicationURL : "//ngs-map.kosnag.ru/"
+    }
+});
