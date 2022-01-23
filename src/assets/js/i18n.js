51c613686e9adb58e3f15cc7b6f2caf90488b00b
kosnag <63012171+kosnag@users.noreply.github.com>
2022-01-24 00:27:10 +0300

    Update i18n.js

--- src/assets/js/i18n.js
+++ src/assets/js/i18n.js
@@ -26,7 +26,7 @@ i18n
        useSuspense: false,
     },
     backend: {
-      loadPath: '/assets/locales/{{lng}}/{{ns}}.json'
+      loadPath: './assets/locales/{{lng}}/{{ns}}.json'
     },
   });
 
