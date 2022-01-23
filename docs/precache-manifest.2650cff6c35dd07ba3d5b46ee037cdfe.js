020828240d12f7d5a46ff7e8a0e21c031afedda3
kosnag <63012171+kosnag@users.noreply.github.com>
2022-01-24 00:20:44 +0300

    Deploy

---
+++ docs/precache-manifest.2650cff6c35dd07ba3d5b46ee037cdfe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5af875925468306cb84bd18f8a5c43",
+    "url": "/NGS_WorldMap_React/index.html"
+  },
+  {
+    "revision": "976046397a442e209bf6",
+    "url": "/NGS_WorldMap_React/static/css/2.7d011da2.chunk.css"
+  },
+  {
+    "revision": "00740fe43da2f4e56758",
+    "url": "/NGS_WorldMap_React/static/css/main.8b2fc730.chunk.css"
+  },
+  {
+    "revision": "976046397a442e209bf6",
+    "url": "/NGS_WorldMap_React/static/js/2.25e07ecd.chunk.js"
+  },
+  {
+    "revision": "3c189f022372ef498421b6b1e392503b",
+    "url": "/NGS_WorldMap_React/static/js/2.25e07ecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00740fe43da2f4e56758",
+    "url": "/NGS_WorldMap_React/static/js/main.ac63c81c.chunk.js"
+  },
+  {
+    "revision": "23dfa8195d7d350f520b",
+    "url": "/NGS_WorldMap_React/static/js/runtime-main.c64a049a.js"
+  },
+  {
+    "revision": "5d1a9d653d0224f40b6216da929bffb2",
+    "url": "/NGS_WorldMap_React/static/media/alphaReactor.5d1a9d65.png"
+  },
+  {
+    "revision": "51c934afb61ee321ae0d9f05a4b432e9",
+    "url": "/NGS_WorldMap_React/static/media/getFetch.51c934af.cjs"
+  },
+  {
+    "revision": "6ac604d00781d38acc7d32d05dde9dcf",
+    "url": "/NGS_WorldMap_React/static/media/ngsfont.6ac604d0.woff"
+  }
+]);
