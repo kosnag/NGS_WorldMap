9944f7d172aad960eb328a4d8c633c92c2134945
kosnag <63012171+kosnag@users.noreply.github.com>
2022-01-28 23:29:18 +0300

    Initialisation through a separate file

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,27 @@ import React, { Fragment, Suspense, useEffect } from 'react';
 import "./assets/js/i18n";
 import { useTranslation } from "react-i18next";
 
-import Map from "./components/map";
+//import LoadingScreen from "./components/loadscreen";
+
+import Init from "./components/init.jsx";
 import NavBar from "./components/navbar";
 import MenuSettings from "./components/menu/settings";
-import MenuLegend from "./components/menu/legend";
 
 const App = () => {
   const {t} = useTranslation("ui");
-
-  const MenuLegendInit = () => {
-    if (localStorage.getItem("settings")) {
-      return <MenuLegend/>;
-    } else {
-      fetch("./assets/data/settings.json").then(response => response.json().then((data) => localStorage.setItem("settings", JSON.stringify(data))));
-      return <Fragment/>;
-    }
-  }
 
   useEffect(() => {
     document.title = t("ui:page_title")
   });
 
   return (
-    <Fragment>
-      <Map/>
+    <Fragment
+      //fallback={<LoadingScreen/>}
+    >
+      <Init.Map/>
       <NavBar/>
       <container>
-        <MenuLegendInit/>
+        <Init.MenuLegend/>
         <MenuSettings/>
       </container>
     </Fragment>
