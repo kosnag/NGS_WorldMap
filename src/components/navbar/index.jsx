9d1e86a2895c3d31540c57a44399817436ff6b61
kosnag <63012171+kosnag@users.noreply.github.com>
2022-01-29 00:40:53 +0300

    Tranlation for navbar buttons

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -6,42 +6,37 @@ import './../../assets/js/i18n';
 import imageMapLegend from './../../assets/ui/icons/mapLegend.png';
 import imageSettings from './../../assets/ui/icons/settings.png';
 import imageAlphaReactor from './../../assets/ui/icons/alphaReactor.png';
-const imageKofi = "https://storage.ko-fi.com/cdn/cup-border.png";
+
+import { useTranslation } from "react-i18next";
 
 const NavBar = () => {
-    return (
-      <navbar>
-        <buttons>
-          <button 
-            onClick={() => window.open("https://ko-fi.com/kosnag")}
-            //onClick={() => {Functions.menuShowHide("menu-donate")}}
-          >
-            <img alt="" src={imageKofi}/>
-            <div>Kofi</div>
-          </button>
-          <button 
-            onClick={() => 
-              window.open("https://twitter.com/search?q=%22%23AlphaReactors%20%23%E3%82%A2%E3%83%AB%E3%83%95%E3%82%A1%E3%83%AA%E3%82%A2%E3%82%AF%E3%82%BF%E3%83%BC%20%23PSO2NGS%22%20(from%3AAphyAmarantha)&f=live", "_blank")
-            }
-          >
-            <img alt="" src={imageAlphaReactor}/>
-            <div>Alpha Reactors</div>
-          </button>
-          <button 
-            onClick={() => Functions.menuShowHide("menu-legend")}
-          >
-            <img alt="" src={imageMapLegend}/>
-            <div>Map Legend</div>
-          </button>
-          <button 
-            onClick={() => Functions.menuShowHide("menu-settings")}
-          >
-            <img alt="" src={imageSettings}/>
-            <div>Options</div>
-          </button>
-        </buttons>
-      </navbar>
-    );
+  const {t} = useTranslation("ui");
+  return (
+    <navbar>
+      <buttons>
+        <button onClick={() => Functions.menuShowHide("menu-support")}>
+          <img alt="" src={"https://storage.ko-fi.com/cdn/cup-border.png"}/>
+          <div>{t("ui:NavBar.support")}</div>
+        </button>
+        <button 
+          onClick={() => 
+            window.open("https://twitter.com/search?q=%22%23AlphaReactors%20%23%E3%82%A2%E3%83%AB%E3%83%95%E3%82%A1%E3%83%AA%E3%82%A2%E3%82%AF%E3%82%BF%E3%83%BC%20%23PSO2NGS%22%20(from%3AAphyAmarantha)&f=live", "_blank")
+          }
+        >
+          <img alt="" src={imageAlphaReactor}/>
+          <div>{t("ui:NavBar.alphaReactor")}</div>
+        </button>
+        <button onClick={() => Functions.menuShowHide("menu-legend")}>
+          <img alt="" src={imageMapLegend}/>
+          <div>{t("ui:NavBar.mapLegend")}</div>
+        </button>
+        <button onClick={() => Functions.menuShowHide("menu-settings")}>
+          <img alt="" src={imageSettings}/>
+          <div>{t("ui:NavBar.options")}</div>
+        </button>
+      </buttons>
+    </navbar>
+  );
 };
 
 export default NavBar;
