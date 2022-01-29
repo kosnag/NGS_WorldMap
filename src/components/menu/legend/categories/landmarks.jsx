0c2383d014eba01c234d3502090c966de2c50f42
kosnag <63012171+kosnag@users.noreply.github.com>
2022-01-29 10:03:02 +0300

    Delete landmarks.jsx

--- src/components/menu/legend/categories/landmarks.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useEffect } from 'react';
-import Checkbox from "react-custom-checkbox";
-import Functions from '../../../assets/js/functions';
-
-const Landmarks = () => {
-    return 
-}
