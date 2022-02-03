7a00e1f2c424ed64c8abda392bf7a889e82364c0
kosnag <63012171+kosnag@users.noreply.github.com>
2022-02-03 10:43:37 +0300

    Update index.php

--- public/mapping/index.php
+++ public/mapping/index.php
@@ -5,14 +5,10 @@ $query = $mysql -> query("select session from kosnag_map.".$datatables["auth"]["
 $session_check = $query -> fetch_assoc();
 
 if (isset($session_check["session"])){
-
 	$query = $mysql -> query("select ".$datatables["auth"]["sessions"].".id,session from kosnag_map.".$datatables["auth"]["sessions"]." inner join kosnag_map.".$datatables["auth"]["users"]." on kosnag_map.".$datatables["auth"]["sessions"].".id=kosnag_map.".$datatables["auth"]["users"].".id where session='".$_COOKIE["session"]."' ");
 	$session_data = $query -> fetch_assoc();
 	include "edit.php";
-
 } else {
-
 	include "auth.html";
-
 }
 ?>
