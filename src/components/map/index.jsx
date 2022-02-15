4862dd074ad162ab73176835543614d82e66a9df
kosnag <63012171+kosnag@users.noreply.github.com>
2022-02-16 02:42:19 +0300

    Init moved into map

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -3,31 +3,35 @@ import L from 'leaflet';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 import "./style.scss";
-import Init from './init';
 
-const boundNorth = 0; 
-const boundEast = 12288/6;
-const boundWest = 0;
-const boundSouth = -12288/6;
-//          boundNorth
-//
-//boundWest             boundEast
-//
-//          boundSouth
+import Landmarks from './markers/landmarks';
 
-const bounds = [[boundSouth, boundWest], [boundNorth, boundEast]];
-const maxbounds = [[boundSouth-100, boundWest], [boundSouth/2+250, boundEast]];
-const center = [boundSouth/4*3, boundEast/2];
 
+function Init(){
+    return (<>
+        <Landmarks/>
+    </>)
+}
 export default function Map(){
+    const bounds = {
+        North: 0,
+        East: 2048,
+        West: 0,
+        South: -2048
+        //      North
+        //
+        //West          East
+        //
+        //      South
+    }
     return (
         <MapContainer
             zoom={0}
             minZoom={0}
             maxZoom={3}
             crs={L.CRS.Simple}
-            maxBounds={maxbounds}
-            center={center}
+            maxBounds={[[bounds.South-100,bounds.West-100],[bounds.South/2+250,bounds.East+100]]}
+            center={[bounds.South/4*3,bounds.East/2]}
             zoomControl={false}
             attributionControl={false}
             keyboard={false}
@@ -36,7 +40,7 @@ export default function Map(){
         >
             <TileLayer
                 url="./assets/images/tiles/{z}/{y}-{x}.png"
-                bounds={bounds}
+                bounds={[[bounds.South,bounds.West],[bounds.North,bounds.East]]}
                 tileSize={1024}
                 noWrap={true}
             />
