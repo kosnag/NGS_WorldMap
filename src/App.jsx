7a788610b606ab5b13c36adbb2ddfcae09701a22
kosnag <63012171+kosnag@users.noreply.github.com>
2022-02-19 10:38:56 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from "react-i18next";
 
 import NavBar from "./components/navbar";
