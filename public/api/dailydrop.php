7a37941d68acda05a66bbb08c11b81292ffac0e2
kosnag <63012171+kosnag@users.noreply.github.com>
2022-05-13 01:27:17 +0300

    Update dailydrop.php

--- public/api/dailydrop.php
+++ public/api/dailydrop.php
@@ -3,7 +3,7 @@ include "mysql.php";
 
 if ($_GET["rgertyrtujrthtgyuky7uktrgyer"] = "nerfgjhrefgsgdtpoghitpjh"){
     $mysql->query("delete from other__alphareactor");
-    $mysql->query("delete from minerals__photonscale");
+    $mysql->query("delete from mineral__photonscale");
 } else {
     header("Location: https://map.pso2.ru/");
 }
