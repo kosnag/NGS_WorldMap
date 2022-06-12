8313d3a380e787a50453fb9ae10ced837a03d9a3
kosnag <63012171+kosnag@users.noreply.github.com>
2022-06-12 20:05:11 +0300

    Fixed bug of not working description for food items

--- src/components/menu/legend/index.jsx
+++ src/components/menu/legend/index.jsx
@@ -61,11 +61,14 @@ export default function MenuLegend(){
           setPreviewIcon("./assets/images/icons/food/"+props.item+".png")
           setPreviewRarity(props.rarity)
           setPreviewTitle(t("items:food."+props.item))
-          setPreviewDescription(
+          {if(props.notable === true){return(setPreviewDescription(
+            t("ui:Map.type")+": "+t("ui:Map.foodType."+props.type)+"\n"+
+            t("items:food.description.prefix."+props.prefix)+" / "+t("items:food.description.type."+props.type)+"\n"+
+            t("items:food.description.prefix.notable")
+          ))} else {return(setPreviewDescription(
             t("ui:Map.type")+": "+t("ui:Map.foodType."+props.type)+"\n"+
             t("items:food.description.prefix."+props.prefix)+" / "+t("items:food.description.type."+props.type)
-            (props.notable === true ? "\n"+t("items:food.description.prefix.notable") : "")
-          )
+          ))}}
         }}
         ><Checkbox icon={<span/>}
         checked={checkLocalStorage("food", props.item)}
