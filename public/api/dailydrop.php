2ff74a37e11fb59d1d94fe975b937e2af821cb82
kosnag <63012171+kosnag@users.noreply.github.com>
2022-06-15 03:32:47 +0300

    Update dailydrop.php

--- public/api/dailydrop.php
+++ public/api/dailydrop.php
@@ -4,10 +4,6 @@ include "mysql.php";
 switch ($_GET["DELETED"]){
 	case "DELETED":
 		$mysql->query("delete from other__alphareactor");
-		$mysql->query("delete from mineral__photonscale");
-		break;
-	case "DELETED":
-		$mysql->query("delete from mineral__randomite");
 		break;
 	default:
 		header("Location: https://map.pso2.ru/");
