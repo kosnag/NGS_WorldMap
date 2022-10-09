83c739d0a6a9f350356ec502514599cd41655a82
kosnag <63012171+kosnag@users.noreply.github.com>
2022-10-09 12:20:51 +0300

    Update mag.jsx

--- src/components/map/markers/landmarks/mag.jsx
+++ src/components/map/markers/landmarks/mag.jsx
@@ -11,16 +11,16 @@ export default function Mag(){
         var i = setInterval(()=>setMarker(window.localStorage_Settings.landmark.mag));
         return ()=>clearInterval(i);
     });
-    useEffect(()=>{marker === 1 ? fetch("./api/read.php?table=landmark__mag").then(response=>response.json()).then(d=>setData(d)) : setData([])},[marker]);
+    useEffect(()=>{marker === 1 ? fetch("//raw.githubusercontent.com/kosnag/NGS_WorldMap/master/public/assets/data/mags.json").then(response=>response.json()).then(d=>setData(d)) : setData([])},[marker]);
     if(data !== null){return(marker ? (data.map((x=>
         <Marker icon={iconLib.mag} position={[x.lat,x.lng]}>
             <Tooltip direction='top'><tooltipwindow>
             <header>
-                <span><menuicon/> {t("mags:type."+x.string)}</span>
+                <span><menuicon/> {t("mags:type."+x.id)}</span>
             </header>
             <content>
                 {t("items:landmark.mag.title")}
-                <id>ID: {x.string}</id>
+                <id>ID: {x.id}</id>
             </content>
             </tooltipwindow></Tooltip>
         </Marker>
