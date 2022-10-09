89fb05f99a62d17aadae3b8a647f0c743b91fc4c
kosnag <63012171+kosnag@users.noreply.github.com>
2022-10-09 12:02:11 +0300

    Added new ryuker device

--- src/components/map/markers/landmarks/ryuker.jsx
+++ src/components/map/markers/landmarks/ryuker.jsx
@@ -11,16 +11,16 @@ export default function Ryuker(){
         var i = setInterval(()=>setMarker(window.localStorage_Settings.landmark.ryuker));
         return ()=>clearInterval(i);
     });
-    useEffect(()=>{marker === 1 ? fetch("./api/read.php?table=landmark__ryuker").then(response=>response.json()).then(d=>setData(d)) : setData([])},[marker]);
+    useEffect(()=>{marker === 1 ? fetch("./api/read.php?table=//raw.githubusercontent.com/kosnag/NGS_WorldMap/master/public/assets/data/ryukers.json").then(response=>response.json()).then(d=>setData(d)) : setData([])},[marker]);
     if(data !== null){return(marker ? (data.map((x=>
         <Marker icon={iconLib.ryuker} position={[x.lat,x.lng]}>
             <Tooltip direction='top'><tooltipwindow>
                 <header>
-                    <span><menuicon/> {t("ryukers:"+x.string)}</span>
+                    <span><menuicon/> {t("ryukers:"+x.id)}</span>
                 </header>
                 <content>
                     {t("items:landmark.ryuker.title")}
-                    <id>ID: {x.string}</id>
+                    <id>ID: {x.id}</id>
                 </content>
             </tooltipwindow></Tooltip>
         </Marker>
