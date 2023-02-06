eec07b1c13d95d264aad85109cd8425a8aa2c0e3
kosnag <im@kosnag.ru>
2023-02-06 21:02:57 +0300

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -5,20 +5,22 @@ const Functions = {
         if (document.getElementById('menu-legend').classList.contains('hidden')) {
           document.getElementById('menu-legend').classList.remove('hidden');
           document.getElementById('menu-settings').classList.add('hidden');
-          document.getElementById('menu-donate').classList.add('hidden');
+          //document.getElementById('menu-donate').classList.add('hidden');
         } else {
           document.getElementById('menu-legend').classList.add('hidden');
         }
         break;
+      
       case 'menu-settings':
         if (document.getElementById('menu-settings').classList.contains('hidden')) {
           document.getElementById('menu-settings').classList.remove('hidden');
           document.getElementById('menu-legend').classList.add('hidden');
-          document.getElementById('menu-donate').classList.add('hidden');
+          //document.getElementById('menu-donate').classList.add('hidden');
         } else {
           document.getElementById('menu-settings').classList.add('hidden');
         }
         break;
+      /*
       case 'menu-donate':
         if (document.getElementById('menu-donate').classList.contains('hidden')) {
           document.getElementById('menu-donate').classList.remove('hidden');
@@ -28,6 +30,7 @@ const Functions = {
           document.getElementById('menu-donate').classList.add('hidden');
         }
         break;
+      */
       default:
         break;
     }
